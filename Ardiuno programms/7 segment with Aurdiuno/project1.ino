baab0aab73ec6f7581f9887d33fdd1f2c46efb06
Meesam <113838998+imeesam@users.noreply.github.com>
2024-03-29 23:54:38 +0500

    Updated...

--- Ardiuno programms/7 segment with Aurdiuno/project1.ino
+++ Ardiuno programms/7 segment with Aurdiuno/project1.ino
@@ -2,22 +2,16 @@
 SevSeg sevseg;
 void setup()
 {
-      //Set to 1 for single-digit display
       byte numDigits = 1;
-      //defines common pins while using multi-digit display. Left for single digit display
       byte digitPins[] = {};
-      //Defines Arduino pin connections in order: A, B, C, D, E, F, G, DP
       byte segmentPins[] = {9,8, 7, 6, 5, 4, 3, 2};
-      byte displayType = COMMON_CATHODE; //Use COMMON_ANODE for Common Anode display
-      bool resistorsOnSegments = true; //‘false’ if resistors are connected to common pin
-      //Initialize sevseg object. Use COMMON_ANODE instead of COMMON_CATHODE for CA display
+      byte displayType = COMMON_CATHODE; 
+      bool resistorsOnSegments = true; 
       sevseg.begin(displayType, numDigits, digitPins, segmentPins, resistorsOnSegments);
       sevseg.setBrightness(30);
 }
 void loop()
-{
-   //Display numbers 0-9 with 1 seconds delay
-   for(int i = 9; i >= 0; i--)
+{  for(int i = 9; i >= 0; i--)
    {
      if (i == 10)
 {
